cb1dda40956de96009e8a92c876413eef88e0555
Danny Yang <yang_danny19@hotmail.com>
2024-07-14 13:30:02 -0700

    initial commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+FILE=.env
+if [ ! -f "$FILE" ]; then
+  echo "$FILE does not exist."
+  exit 1
+fi
+
+docker pull lightyearlabs/lightyear-his:client-latest
+docker run -it -p 3000:3000 -p 4000:4000 --env-file=.env lightyearlabs/lightyear-his:client-latest
