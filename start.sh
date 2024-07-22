0e5f2ae703b507302e78cfd337bab7329d140fa6
Danny Yang <yang_danny19@hotmail.com>
2024-07-22 10:53:05 -0700

    source .env file

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ if [ ! -f "$FILE" ]; then
   exit 1
 fi
 
+. .env
 docker pull lightyearlabs/lightyear-his:$IMAGE_TAG
 docker run -it -p 3000:3000 -p 4000:4000 --env-file=.env lightyearlabs/lightyear-his:$IMAGE_TAG
