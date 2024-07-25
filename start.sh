a3c004bc9caa85520b70ee54686337d4b1faaf84
Danny Yang <yang_danny19@hotmail.com>
2024-07-24 18:28:08 -0700

    create volume

--- start.sh
+++ start.sh
@@ -9,4 +9,10 @@ fi
 
 . .env
 docker pull lightyearlabs/lightyear-his:$IMAGE_TAG
-docker run -it -p 3000:3000 -p 4000:4000 --env-file=.env lightyearlabs/lightyear-his:$IMAGE_TAG
+docker volume create worklist-vol
+docker run -it \
+  -p 3000:3000 \
+  -p 4000:4000 \
+  --env-file=.env \
+  --mount source=worklist-vol,target=/usr/src/app/packages/dicomClient/worklists \
+  lightyearlabs/lightyear-his:$IMAGE_TAG
