8bdf5cdba31fcc2a81d23f314fbcb96169ad3240
Danny Yang <yang_danny19@hotmail.com>
2025-08-12 15:43:12 -0700

    start docker automatically

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+open -a Docker
+
+echo "Waiting for Docker to start..."
+while ! docker system info >/dev/null 2>&1; do
+    sleep 1
+done
+
+echo "Docker is ready, running script..."
+
 git reset --hard
 git pull
 
